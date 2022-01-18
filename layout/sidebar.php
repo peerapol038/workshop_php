60cc2490a6d71f339b712916b4f8639aced63936
peerapol038 <peerapollnw9@gmail.com>
2022-01-18 09:02:59 +0700

    first commit

--- layout/sidebar.php
+++ layout/sidebar.php
@@ -5,7 +5,7 @@
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
       <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">Admin Panel</span>
+      <span class="brand-text font-weight-light">peerapol</span>
     </a>
 
     <!-- Sidebar -->
@@ -16,7 +16,7 @@
           <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Administrator</a>
+          <a href="#" class="d-block">peerapol</a>
         </div>
       </div>
 
